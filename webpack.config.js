2d5bb8a7d334c4dc3dcedd7bc1844cc467b4261d
Jack Callister <jack.callister@quillcontent.com>
2015-10-20 15:41:25 +0100

    Correct external packaging.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,23 @@
 var path = require('path')
 
-var reactExternal = {
-  root: 'React',
-  commonjs2: 'react',
-  commonjs: 'react',
-  amd: 'react'
-};
-
 module.exports = {
   entry: './index.js',
 
   output: {
     path: path.join(__dirname, 'lib'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'react-lazy-locales',
+    libraryTarget: 'umd'
   },
 
   externals: {
-    'react': reactExternal,
+    'react': 'react'
   },
 
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['babel-loader'], exclude: /node_modules/ }
     ]
-  },
-
-  output: {
-    library: 'ReactLazyLocales',
-    libraryTarget: 'umd'
   },
 
   resolve: {
